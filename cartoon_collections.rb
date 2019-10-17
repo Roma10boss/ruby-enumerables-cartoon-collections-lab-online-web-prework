278bc0f139ea3b1bda5021bc239f33d9409713f4
Romario Gustave <romagustave10@gmail.com>
2019-10-17 19:42:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ end
 def long_planeteer_calls# code an argument here
   # Your code here 
   short_words = ["puff", "go", "two"]
-  
+  long_planeteer_calls(short_words)
 end
 
 def find_the_cheese# code an argument here
