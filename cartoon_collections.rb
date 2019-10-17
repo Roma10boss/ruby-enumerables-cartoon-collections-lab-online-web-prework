e7112eface6d87779c58e34a11ce47b00363aefe
Romario Gustave <romagustave10@gmail.com>
2019-10-17 19:19:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves# code an argument here
   # Your code here
-  array =[]
+  array =["Doc", "Dopey", "Bashful", "Grumpy"]
+  puts array.each 
 end
 
 def summon_captain_planet# code an argument here
