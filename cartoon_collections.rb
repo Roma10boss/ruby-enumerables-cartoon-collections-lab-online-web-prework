f8241a34ed456d31651d98c6e469dfc3e3b98a29
Romario Gustave <romagustave10@gmail.com>
2019-10-17 19:39:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,8 @@ end
 def summon_captain_planet# code an argument here
   # Your code here
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  planeteer_calls.map ! {|planeteer_calls| planeteer_calls.upcasenames.each { |name| puts name + ' is a programmer' }
+  planeteer_calls.map ! {|planeteer_calls| planeteer_calls.upcase}
+ planeteer_calls.each { |planeteer_calls| puts planeteer_calls + '!' }
   puts planeteer_calls
 end
 
